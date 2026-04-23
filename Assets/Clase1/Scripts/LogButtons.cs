320c541d5aac04da2b112c6844f08bc09e694754
Merck92 <marcialfgutierrez@gmail.com>
2026-04-22 22:53:25 -0300

    TP1 v1
    Viene con todo lo que me pidió, excepto poner la pausa.

--- Assets/Clase1/Scripts/LogButtons.cs
+++ Assets/Clase1/Scripts/LogButtons.cs
@@ -3,16 +3,19 @@ using UnityEngine.UI;
 
 public class LogButtons : MonoBehaviour
 {
+
     // TAREA 1: Haz que este botón muestre un mensaje normal en consola (color blanco)
     public void ShowLogInfoMessage()
     {
         // Escribe tu código aquí:
+        Debug.Log("Todavía no está nevando.");
     }
 
     // TAREA 2: Haz que este botón muestre una ADVERTENCIA (triángulo amarillo)
     public void ShowLogWarningMessage()
     {
         // Escribe tu código aquí:
+        Debug.Log("Esa acción va a ocacionar que los Manos nos detecten.");
     }
 
     // TAREA 3: Haz que este botón muestre un ERROR (octágono rojo)
@@ -20,5 +23,7 @@ public class LogButtons : MonoBehaviour
     public void ShowLogErrorMessage()
     {
         // Escribe tu código aquí:
+        Debug.Log("¡ELLOS SABEN DONDE ESTAMOS!");
+        //Marcial: Intenté con EditorApplication.Exit o Apliccation.Quit() pero ninguno me salió. También probé con el time, pero no me salió.
     }
 }
